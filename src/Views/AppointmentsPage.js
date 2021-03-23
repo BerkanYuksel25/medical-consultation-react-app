44b18b6c097a07e4af4815b601ba4f29b0727a08
keysers0ze <kevinguemail@gmail.com>
2021-03-23 23:24:49 +1100

    Added calender to appointments

--- src/Views/AppointmentsPage.js
+++ src/Views/AppointmentsPage.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
-import { auth } from "../Services/firebase";
-import HealthAssistant from "../Components/Chatbot/HealthAssistant";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
 
 export default class AppointmentsPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      events: [
+        {
+          start: moment().toDate(),
+          end: moment()
+            .add(1, "days")
+            .toDate(),
+          title: "Some title"
+        }
+      ]
+    };
   
     }
   
@@ -12,15 +28,14 @@ export default class AppointmentsPage extends Component {
 
   render() {
     return (
-      <div align="center">
-        Dashboard
-        <div>
-      
-        </div>
-        <button style={this.submitButton} onClick={this.handleSignout}>
-          Log out
-        </button>
-        <HealthAssistant />
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
       </div>
     );
   }
