7513c1fe0581901abffd45341ca4a2721dfd75eb
tanmaymunigala <tanmay.munigala@gmail.com>
2021-03-30 19:06:23 +1100

    Refactor and add js config

--- src/Components/Chatbot/HealthAssistant.js
+++ src/Components/Chatbot/HealthAssistant.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ChatBot from "react-simple-chatbot";
-import PropTypes from "prop-types";
 import { ThemeProvider } from "styled-components";
+import Review from "./Review";
 
 const theme = {
   background: "#f5f8fb",
@@ -19,118 +19,6 @@ const config = {
   width: "400px",
   height: "500px",
   floating: true,
-};
-
-class Review extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: "",
-      sex: "",
-      age: "",
-      headaches: "",
-      fever: "",
-      covidContact: "",
-      soreThroat: "",
-      shortnessOfBreath: "",
-      cough: "",
-    };
-  }
-
-  componentWillMount() {
-    const { steps } = this.props;
-    const {
-      name,
-      sex,
-      age,
-      headaches,
-      fever,
-      soreThroat,
-      cough,
-      shortnessOfBreath,
-      covidContact,
-    } = steps;
-
-    this.setState({
-      name,
-      sex,
-      age,
-      headaches,
-      fever,
-      soreThroat,
-      cough,
-      shortnessOfBreath,
-      covidContact,
-    });
-  }
-
-  render() {
-    const {
-      name,
-      sex,
-      age,
-      headaches,
-      fever,
-      soreThroat,
-      cough,
-      shortnessOfBreath,
-      covidContact,
-    } = this.state;
-    return (
-      <div style={{ width: "100%" }}>
-        <h3>Summary</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td>Name</td>
-              <td>{name.value}</td>
-            </tr>
-            <tr>
-              <td>Gender</td>
-              <td>{sex.value}</td>
-            </tr>
-            <tr>
-              <td>Age</td>
-              <td>{age.value}</td>
-            </tr>
-            <tr>
-              <td>Headaches</td>
-              <td>{headaches.value}</td>
-            </tr>
-            <tr>
-              <td>Fever</td>
-              <td>{fever.value}</td>
-            </tr>
-            <tr>
-              <td>Sore Throat</td>
-              <td>{soreThroat.value}</td>
-            </tr>
-            <tr>
-              <td>Cough</td>
-              <td>{cough.value}</td>
-            </tr>
-            <tr>
-              <td>Shortness Of Breath</td>
-              <td>{shortnessOfBreath.value}</td>
-            </tr>
-            <tr>
-              <td>Contact with COVID</td>
-              <td>{covidContact.value}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-Review.propTypes = {
-  steps: PropTypes.object,
-};
-
-Review.defaultProps = {
-  steps: undefined,
 };
 
 // For component documentation, please refer: https://lucasbassetti.com.br/react-simple-chatbot/
@@ -183,7 +71,7 @@ class HealthAssistant extends Component {
               validator: (value) => {
                 if (isNaN(value)) {
                   return "value must be a number";
-                } else if (value < 0) {
+                } else if (value < 1) {
                   return "value must be positive";
                 } else if (value > 120) {
                   return `${value}? Come on!`;
