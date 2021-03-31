8c3ba0846d9498aa4088a6a82259efa2bc9248a2
berkan_yuksel25 <Berkan.Yuksel@student.uts.edu.au>
2021-03-31 18:16:23 +1100

    Fixing refs issue with gender selector.

--- src/Views/AppointmentsPage.js
+++ src/Views/AppointmentsPage.js
@@ -5,14 +5,12 @@ import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { withStyles } from "@material-ui/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 import SubmitButton from "../Components/SubmitButton";
@@ -55,7 +53,6 @@ class AppointmentsPage extends Component {
     this.setState({ open: false });
   };
   render() {
-    const { classes } = this.props;
     return (
       <div className="App">
         <Grid container spacing={3}>
