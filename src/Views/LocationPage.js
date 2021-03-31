8de603ec8af6401a7fbe4decac7f63f788e14b57
Jakeb Gutierrez <drwaffle69@gmail.com>
2021-03-31 15:00:02 +1100

    began google map api implementation

--- src/Views/LocationPage.js
+++ src/Views/LocationPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GlobalLayout from "../Components/GlobalLayout";
+import GoogleMapReact from 'google-map-react'
 import { Typography, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -7,6 +8,16 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(4),
   },
 }));
+
+const location = {
+  address: 'University of Technology Sydney',
+  lat: 33.8832,
+  lng: 151.2005,
+}
+
+const handleApiLoaded = (map, maps) => {
+  // use map and maps objects
+};
 
 export default function LocationPage() {
   const classes = useStyles();
