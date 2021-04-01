2bcaa7e77a44651e2e04b1d8504ba5214a7cf9cc
Campbell-Uni <campbell.m.gorman@student.uts.edu.au>
2021-04-01 17:51:59 +1100

    Updated camera / model / lighting positioning
     A quick note; I also removed the default body padding so you don't get scroll bars
    
     Essentially what I have done here is placed the skeleton at the origin (0, 0, 0) and then moved the camera and key light back 100 units on the z plane.

--- src/Views/3D model/js/main.js
+++ src/Views/3D model/js/main.js
@@ -5,53 +5,49 @@ let keyLight, fillLight, backLight;
 let cameraLight, ambientLight;
 
 function init() {
-  scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-  document.body.appendChild(renderer.domElement);
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.campingFactor = 0.25;
-  controls.enableZoom = true;
+    document.body.appendChild(renderer.domElement);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.campingFactor = 0.25;
+    controls.enableZoom = true;
 
-  // remember lighting later
-  cameraLight = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.5);
-  camera.add(cameraLight);
-  // camera.position.set(0, 20,100);
-  // controls.update(); 
-  
-  scene.add(camera);
-  ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.2);
-  scene.add(ambientLight);
+    // remember lighting later
+    cameraLight = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.5);
+    camera.add(cameraLight);
+    camera.position.set(0, 0, 100);
+    controls.update();
 
-  //   keyLight = new THREE.DirectionalLight(new THREE.Color("hsl(30, 100%, 75%)"), 1.0);
-  //   keyLight.position.set(-100,0,100);
+    scene.add(camera);
+    ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.2);
+    scene.add(ambientLight);
 
-  //   fillLight = new THREE.DirectionalLight(new THREE.)
+    keyLight = new THREE.DirectionalLight(new THREE.Color("hsl(30, 100%, 75%)"), 1.0);
+    keyLight.position.set(0, 0, 100);
 
-  //loading the 3D model object will need node command here on in.
-  // objLoader = new THREE.OBJLoader();
-  // objLoader.setPath("/models/");
-  // objLoader.load("skeleton.obj", function (object) {
-  //   // Setting object back 60 from camera
-  //   object.position.y -= 100;
-  //   scene.add(object);
-  // });
+
+    //loading the 3D model object will need node command here on in.
+    objLoader = new THREE.OBJLoader();
+    objLoader.load("./models/skeleton.obj", function (object) {
+        scene.add(object);
+    });
 }
 
 function animate() {
-  controls.update();
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+    controls.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
 init();
