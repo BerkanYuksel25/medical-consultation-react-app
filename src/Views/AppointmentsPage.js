11631a9f18db56675137ed758bafc0d029266d5a
KevinGu <yiming.gu@accenture.com>
2021-04-13 21:51:38 +1000

    changes to appointment

--- src/Views/AppointmentsPage.js
+++ src/Views/AppointmentsPage.js
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
 
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+});
 
 const localizer = momentLocalizer(moment);
 
-export default class AppointmentsPage extends Component {
-  constructor(props) {
-    super(props);
+class AppointmentsPage extends React.Component {
+  constructor() {
+    super();
     this.state = {
       events: [
         {
@@ -24,63 +38,30 @@ export default class AppointmentsPage extends Component {
           end: moment()
             .add(1, "days")
             .toDate(),
-          title: "Some title"
-        }
+          title: "Some title",
+        },
       ],
       open: false,
     };
   }
 
-  handleClickOpen = () => {
-    this.setState({open: true});
-  }
-  
-  handleClose = () => {
-    this.setState({open: false});
-  }
-
   render() {
+    const { classes } = this.props;
     return (
-			<div className="App">
-				<Calendar
-					localizer={localizer}
-					defaultDate={new Date()}
-					defaultView="month"
-					events={this.state.events}
-					style={{ height: "1000px", width: "1000px" }}
-				/>
-				<Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-					Add Appointment
-				</Button>
-				<Dialog
-					open={this.state.open}
-					onClose={this.handleClose}
-					aria-labelledby="form-dialog-title"
-				>
-					<DialogTitle id="form-dialog-title">Add Appointment</DialogTitle>
-					<DialogContent>
-            {/* <DialogContentText>Date of Appointment</DialogContentText> */}
-						<TextField
-							margin="dense"
-							id="date"
-              label="Appointment Date"
-							type="datetime-local"
-              defaultValue={(new Date().getFullYear())+"-"+(new Date().getMonth()+1)+"-"+(new Date().getDate())+"T"+ (new Date().getHours())+":"+(new Date().getMinutes())}
-              InputLabelProps={{
-                shrink: true,
-              }}
-						/>
-					</DialogContent>
-					<DialogActions>
-						<Button onClick={this.handleClose} color="primary">
-							Cancel
-						</Button>
-						<Button onClick={this.handleClose} color="primary">
-							Add Appointment
-						</Button>
-					</DialogActions>
-				</Dialog>
-			</div>
-		);
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <div>
+              <Calendar localizer={localizer} style={{ height: 500 }} />
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
+
+export default withStyles(styles)(AppointmentsPage);
