e100264d04551eee33ef62af89e0caa53e3a95a8
12566434 <zainsalimuddin@gmail.com>
2021-04-14 12:27:14 +1000

    updated branch with lighting/camera changes

--- src/Views/V3/js/main.js
+++ src/Views/V3/js/main.js
@@ -1,56 +1,53 @@
-let scene, camera, renderer;            //all stuff needed to generate models needed
-let controls;                           
+let scene, camera, renderer;
+let controls;
 let objLoader;
 let keyLight, fillLight, backLight;
 let cameraLight, ambientLight;
 
 function init() {
-  scene = new THREE.Scene();                        //created a new scene
+    scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(             //created a new camera with certain dimensions
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });      //renders the graphic image. makes it more smooth
-  renderer.setSize(window.innerWidth, window.innerHeight);      
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-  document.body.appendChild(renderer.domElement);                     //appends to the document? what document?
-  controls = new THREE.OrbitControls(camera, renderer.domElement);    //controls used for the cameras
-  controls.enableDamping = true;
-  controls.campingFactor = 0.25;
-  controls.enableZoom = true;                                         //zoom in to the model
+    document.body.appendChild(renderer.domElement);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.campingFactor = 0.25;
+    controls.enableZoom = true;
 
-  // remember lighting later
-  cameraLight = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.5);  //this is just lighting
-  camera.add(cameraLight);
-  
-  
-  scene.add(camera);
-  ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.2);
-  scene.add(ambientLight);
+    // remember lighting later
+    cameraLight = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.5);
+    camera.add(cameraLight);
+    camera.position.set(0, 0, 100);
+    controls.update();
 
-  //   keyLight = new THREE.DirectionalLight(new THREE.Color("hsl(30, 100%, 75%)"), 1.0);
-  //   keyLight.position.set(-100,0,100);
+    scene.add(camera);
+    ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.2);
+    scene.add(ambientLight);
 
-  //   fillLight = new THREE.DirectionalLight(new THREE.)
+    keyLight = new THREE.DirectionalLight(new THREE.Color("hsl(30, 100%, 75%)"), 1.0);
+    keyLight.position.set(0, 0, 100);
 
-  //loading the 3D model object will need node command here on in.
-  objLoader = new THREE.OBJLoader();
-  objLoader.setPath("/models/");
-  objLoader.load("skeleton.obj", function (object) {            //attempting to load the skeleton model in the models folder
-    // Setting object back 60 from camera
-    object.position.y -= 10;                                    //not sure whats going on here. setting the model down?
-    scene.add(object);                                          //adding object to the scene
-  });
+
+    //loading the 3D model object will need node command here on in.
+    objLoader = new THREE.OBJLoader();
+    objLoader.load("./models/skeleton.obj", function (object) {
+        scene.add(object);
+    });
 }
 
-function animate() {                                            //animate object
-  controls.update();                                              
-  requestAnimationFrame(animate); 
-  renderer.render(scene, camera);                                //renders animation on the scene
+function animate() {
+    controls.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
 init();
