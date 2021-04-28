774ad5ff4c508d3a979f77c6dc640930e83478bb
Krijan21 <krijancool.prajapati@gmail.com>
2021-04-28 10:31:52 +1000

    about page

--- src/Views/AboutPage.js
+++ src/Views/AboutPage.js
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function AboutPage() {
-  return <></>;
+  return(
+  <div>
+  <h2>About</h2>
+
+  <p>
+  In current pandemic situation we have experienced large number of people getting affected by the outbreak of covid-19.
+  It is equally important that we ensure that whoever is affected by this outbreak gets the ability to recover. 
+  However, at the same time we need to ensure that they recover in very safe manner without affecting any other people due to its contagious nature of the virus. 
+  Therefore, we have proposed the idea of creating software system that has ability to detect if an individual has covid-19. 
+  Through designing interactive website that can allow user to chat with an automated chatbot system which can output answer according to their question 
+  will allow user to utilize the functionality anytime of the day. 
+
+  <li> Existing solutions are comprehensive and bulky diagnosis and online doctor solutions </li> 
+
+  <li> Will help to lighten the load on the Australian healthcare industry by reducing the number of unnecessary COVID tests. </li>
+
+  <li> One of the first Australian AI medical apps </li>
+  </p>
+
+
+  </div>
+  );
 }
