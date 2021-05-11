995c2d89e9f29df25649153b610c34721a7692f7
KevinGu <yiming.gu@accenture.com>
2021-05-12 00:11:54 +1000

    add dynamic cards, form details

--- src/Views/AppointmentsPage.js
+++ src/Views/AppointmentsPage.js
@@ -20,7 +20,11 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { ThreeSixty } from "@material-ui/icons";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+// import { ThreeSixty } from "@material-ui/icons";
 
 const styles = (theme) => ({
   root: {
@@ -38,14 +42,8 @@ class AppointmentsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Get current user data
       user: JSON.parse(localStorage.getItem("user")),
-
-      currentDateTime: new Date(),
-      curDateTime: moment(props.date).format(new Date().toLocaleString()),
-
-      txt_booking_title: "",
-      txt_booking_time: "",
-
       events: [
         {
           //start: new Date(),
@@ -56,30 +54,116 @@ class AppointmentsPage extends Component {
       ],
       open: false,
       apptDateTime: new Date(),
+      apptType: "GP",
       apptTitle: "Example appointment",
       apptDocName: "Dr Nick",
-      apptDescription: "",
+      apptDescription: "Dr Nick will check you out",
+      apptLocation: "Dr Nick's house",
       apptList: [],
-	  editing: false,
-	  eventKey: new Date(),
+      editing: false,
+      eventKey: new Date(),
+
+      // currentDateTime: new Date(),
+      // curDateTime: moment(props.date).format(new Date().toLocaleString()),
+
+      // txt_booking_title: "",
+      // txt_booking_time: "",
     };
   }
 
-  componentDidMount() {
-    // get appointments from firebase db
-    // updates when db is changed or on start
-    var db = database().ref("appointments/" + this.state.user.uid + "/");
-    db.on("value", (data) => {
+  // componentDidMount() {
+  //   // gets appointments from firebase db
+  //   // updates when db is changed or on start
+  //   var db = database().ref("appointments/" + this.state.user.uid + "/");
+  //   db.on("value", (data) => {
+  //     try {
+  //       var appointmentsDB = data.val();
+  //       console.log(appointmentsDB);
+  //       this.setState({ apptList: appointmentsDB });
+  //       console.log(this.apptList);
+  //       var keys = Object.keys(appointmentsDB);
+  //       var tempEvents = [{}];
+
+  //       for (let i = 0; i < keys.length; i++) {
+  //         var k = keys[i];
+  //         var dbApptName = appointmentsDB[k].appointment_name;
+  //         var dbApptTime = appointmentsDB[k].appointment_time;
+  //         var dbApptDesc = appointmentsDB[k].appointment_description;
+  //         //   console.log(dbApptName, dbApptTime);
+  //         tempEvents.push({
+  //           start: new Date(dbApptTime),
+  //           end: new Date(dbApptTime),
+  //           title: dbApptName,
+  //           description: dbApptDesc,
+  //         });
+  //       }
+
+  //       this.setState({ events: tempEvents });
+  //       // this.setState((state) => {
+  //       //   const apptList = state.apptList.concat(tempEvents);
+  //       //   return {
+  //       //     apptList,
+  //       //   };
+  //       // });
+  //       // console.log(this.state.apptList);
+
+  //       for (let i = 0; i < this.tempEvents.length; i++) {
+  //         console.log(tempEvents[i]);
+  //       }
+  //     } catch (err) {
+  //       // console.log("ERROR: no appointments in database");
+  //       console.log("ERROR, details below");
+  //       console.log(err);
+  //     }
+  //   });
+  // }
+
+  componentDidMount = async () => {
+    // Gets appointments from firebase db on start
+    // updates when db is changed
+    const apptsRef = database().ref(
+      "appointments/" + this.state.user.uid + "/"
+    );
+
+    apptsRef.on("value", (snapshot) => {
       try {
-        var appointmentsDB = data.val();
-        var keys = Object.keys(appointmentsDB);
+        // Get data from firebase
+        let appointments = snapshot.val();
+        console.log("Appt refs from DB");
+        console.log(appointments);
+
+        // loop through data and set new state
+        let newApptListState = [];
+        for (let appt in appointments) {
+          newApptListState.push({
+            id: appt,
+            apptTitle: appointments[appt].appointment_name,
+            apptTime: appointments[appt].appointment_time,
+            apptType: appointments[appt].appointment_type,
+            apptDocName: appointments[appt].appointment_doc,
+            apptDescription: appointments[appt].appointment_description,
+            apptLocation: appointments[appt].appointment_location,
+          });
+        }
+        this.setState({ apptList: newApptListState }, function() {
+          // callback so that state can updated ayscn
+          console.log(this.state.apptList);
+        });
+        console.log("Setting apptList state, apptList:");
+
+        // this.setState({ apptList: [appointments] }, function() {
+        //   // callback so that state can updated ayscn
+        //   console.log(this.state.apptList);
+        // });
+
+        var keys = Object.keys(appointments);
         var tempEvents = [{}];
 
         for (let i = 0; i < keys.length; i++) {
           var k = keys[i];
-          var dbApptName = appointmentsDB[k].appointment_name;
-          var dbApptTime = appointmentsDB[k].appointment_time;
-          var dbApptDesc = appointmentsDB[k].appointment_description;
+          var dbApptName = appointments[k].appointment_name;
+          var dbApptTime = appointments[k].appointment_time;
+          var dbApptDesc = appointments[k].appointment_description;
           //   console.log(dbApptName, dbApptTime);
           tempEvents.push({
             start: new Date(dbApptTime),
@@ -89,35 +173,28 @@ class AppointmentsPage extends Component {
           });
         }
 
-		this.setState({ events: tempEvents });
-		this.setState((state) => {
-            const apptList = state.apptList.concat(tempEvents);
-            return {
-              apptList,
-            };
-        });
-        console.log(this.state.apptList);
+        this.setState({ events: tempEvents });
 
-        for (let i = 0; i < this.tempEvents.length; i++) {
-          console.log(tempEvents[i]);
-        }
+        // for (let i = 0; i < this.tempEvents.length; i++) {
+        //   console.log(tempEvents[i]);
+        // }
       } catch (err) {
-        console.log("ERROR: no appointments in database");
+        // console.log("ERROR: no appointments in database");
+        console.log("ERROR, details below");
         console.log(err);
       }
     });
-  }
+  };
 
   handleClickOpen = () => {
     this.setState({ open: true });
   };
 
   handleSubmit = async (event) => {
-	//if editing, delete and then recreate the event
-	if(this.state.editing)
-		this.deleteEvent(this.state.eventKey);
+    //if editing, delete and then recreate the event
+    if (this.state.editing) this.deleteEvent(this.state.eventKey);
 
-	//add to database
+    //add to database
     console.log(this.state.apptDateTime.valueOf());
     console.log(this.state.apptTitle);
     await database()
@@ -130,6 +207,10 @@ class AppointmentsPage extends Component {
       .set({
         appointment_time: this.state.apptDateTime.toISOString(),
         appointment_name: this.state.apptTitle,
+        appointment_type: this.state.apptType,
+        appointment_doc: this.state.apptDocName,
+        appointment_description: this.state.apptDescription,
+        appointment_location: this.state.apptLocation,
       })
       .catch((error) => {
         console.log(error);
@@ -149,19 +230,28 @@ class AppointmentsPage extends Component {
     this.resetDialogBox();
   };
 
+  handleChangeFormApptType = (event) => {
+    this.setState({ apptType: event.target.value });
+  };
+
   handleChangeDateTime = (e) => {
     this.setState({ apptDateTime: new Date(e.target.value) });
   };
 
-  handleChangeTitle = (e) => {
+  handleChangeFormTitle = (e) => {
     this.setState({ apptTitle: e.target.value });
   };
 
   handleChangeFormDocName = (e) => {
     this.setState({ apptDocName: e.target.value });
   };
+
   handleChangeFormDescription = (e) => {
     this.setState({ apptDescription: e.target.value });
+  };
+
+  handleChangeFormLocation = (e) => {
+    this.setState({ apptLocation: e.target.value });
   };
 
   handleSelectSlot = ({ start, end }) => {
@@ -170,58 +260,63 @@ class AppointmentsPage extends Component {
 
   handleSelectEvent = async (e) => {
     //open dialog box with clicked events.
-	this.setState({
-    	apptDateTime: e.start,
-    	apptTitle: e.title,
-    	open: true,
-		editing: true,
-		eventKey: e.start.valueOf(),
+    this.setState({
+      apptDateTime: e.start,
+      apptTitle: e.title,
+      open: true,
+      editing: true,
+      eventKey: e.start.valueOf(),
     });
   };
 
   handleDelete = () => {
-	this.deleteEvent(this.state.apptDateTime);
-	this.resetDialogBox();
-  }
+    this.deleteEvent(this.state.apptDateTime);
+    this.resetDialogBox();
+  };
 
   resetDialogBox = () => {
-	//closes dialog box and reset values
-	this.setState({
-		open: false,
-		apptDateTime: new Date(),
-		apptTitle: "Example appointment",
-		apptDocName: "Dr Nick",
-		apptDescription: "Dr Nick will check you out ;)",
-		editing: false,
-		eventKey: new Date(),
-	  });
-  }
+    //closes dialog box and reset values
+    this.setState({
+      open: false,
+      apptDateTime: new Date(),
+      apptTitle: "Example appointment",
+      apptDocName: "Dr Nick",
+      apptDescription: "Dr Nick will check you out ;)",
+      editing: false,
+      eventKey: new Date(),
+    });
+  };
 
   deleteEvent = async (e) => {
-	//delete event from db
+    //delete event from db
     await database()
-    	.ref(
-    		"appointments/" +
-    			this.state.user.uid +
-    			"/" +
-    			e.valueOf()
-    	)
-    	.remove();
-  }
+      .ref("appointments/" + this.state.user.uid + "/" + e.valueOf())
+      .remove();
+  };
 
-  createAppointmentCards() {}
+  createAppointmentCards() {
+    var appts = this.state.apptList;
+    console.log("create appoitment cards");
+    console.log(appts);
+    return appts.map((appt) => {
+      <div>Test</div>;
+    });
+    // console.log("create appoitment cards");
+    // console.log(appts);
+    // return appts.map((el) => <div>Test lol</div>);
+  }
 
   render() {
     const { classes } = this.props;
-	let deleteDialogButton;
-	if (this.state.editing) {
-		deleteDialogButton = (
-			<Button onClick={this.handleDelete} color="primary">
-				Delete
-			</Button>
-		);
-	}
-	
+    let deleteDialogButton;
+    if (this.state.editing) {
+      deleteDialogButton = (
+        <Button onClick={this.handleDelete} color="primary">
+          Delete
+        </Button>
+      );
+    }
+
     return (
       <div className="App">
         <Grid container spacing={3}>
@@ -243,8 +338,39 @@ class AppointmentsPage extends Component {
               onClick={this.handleClickOpen}
               text="Create Appointment"
             />
+            {/* {this.createAppointmentCards()} */}
+            {this.state.apptList.reverse().map((appt) => {
+              return (
+                <Card className={classes.root}>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      {appt.apptType}
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                      {appt.apptTitle}
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                      {appt.apptDocName}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                      {appt.apptTime}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      {appt.apptDescription}
+                    </Typography>
+                    <CardActions>
+                      <Button size="small">More info</Button>
+                    </CardActions>
+                  </CardContent>
+                </Card>
+              );
+            })}
 
-            <Card className={classes.root}>
+            {/* <Card className={classes.root}>
               <CardContent>
                 <Typography
                   className={classes.title}
@@ -266,23 +392,40 @@ class AppointmentsPage extends Component {
               <CardActions>
                 <Button size="small">More info</Button>
               </CardActions>
-            </Card>
+            </Card> */}
+
             <Dialog
               open={this.state.open}
               onClose={this.handleClose}
               aria-labelledby="form-dialog-title"
             >
-              <DialogTitle id="form-dialog-title">{this.state.editing?"Edit Appointment" : "Add Appointment"}</DialogTitle>
+              <DialogTitle id="form-dialog-title">
+                {this.state.editing ? "Edit Appointment" : "Add Appointment"}
+              </DialogTitle>
               <DialogContent>
                 <DialogContentText>
-                  To {this.state.editing? "edit" : "create"} an appointment, please enter the mandatory fields.			  
+                  To {this.state.editing ? "edit" : "create"} an
+                  appointment,please enter the mandatory fields.
                 </DialogContentText>
                 <TextField
-                  id="title"
-                  label="Name"
+                  id="txt_appt_type"
+                  select
+                  label="Select Appointment Type"
+                  fullWidth
+                  value={this.apptType}
+                  onChange={this.handleChangeFormApptType}
+                  // helperText="Please select your appointment type"
+                >
+                  <MenuItem value={"GP"}>GP</MenuItem>
+                  <MenuItem value={"Specialist"}>Specialist</MenuItem>
+                </TextField>
+
+                <TextField
+                  id="txt_title"
+                  label="Appointment Title"
                   fullWidth
                   value={this.state.apptTitle}
-                  onChange={this.handleChangeTitle}
+                  onChange={this.handleChangeFormTitle}
                 />
                 <TextField
                   autoFocus
@@ -308,27 +451,34 @@ class AppointmentsPage extends Component {
                   onChange={this.handleChangeDateTime}
                 />
                 <TextField
-                  id="form-doc-name"
+                  id="txt_doc_name"
                   label="Physican"
                   fullWidth
                   value={this.state.apptDocName}
                   onChange={this.handleChangeFormDocName}
                 />
                 <TextField
-                  id="form-description"
-                  label="Appointment description"
+                  id="txt_description"
+                  label="Appointment Description"
                   fullWidth
                   value={this.state.apptDescription}
                   onChange={this.handleChangeFormDescription}
                 />
+                <TextField
+                  id="txt_location"
+                  label="Appointment Location"
+                  fullWidth
+                  value={this.state.apptLocation}
+                  onChange={this.handleChangeFormLocation}
+                />
               </DialogContent>
               <DialogActions>
-			  	{deleteDialogButton}
+                {deleteDialogButton}
                 <Button onClick={this.handleCancel} color="primary">
                   Cancel
                 </Button>
                 <Button onClick={this.handleSubmit} color="primary">
-                  {this.state.editing? "Edit Appointment" : "Add Appointment"}
+                  {this.state.editing ? "Edit Appointment" : "Add Appointment"}
                 </Button>
               </DialogActions>
             </Dialog>
